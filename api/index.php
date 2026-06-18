0dd79c4f3c2ee3e772cae11aa7c42a11b41072f4
Genta Halilintar <gentahalilintar36@gmail.com>
2026-06-18 19:10:01 +0700

    chore: update Vercel deployment configuration and clean up Laravel HTTP kernel bootstrap

--- api/index.php
+++ api/index.php
@@ -4,19 +4,15 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// Paksa setup path
 $app->useStoragePath('/tmp');
-$app->useBootstrapPath('/tmp');
 
-try {
-    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-    $response = $kernel->handle($request = Illuminate\Http\Request::capture());
-    $response->send();
-    $kernel->terminate($request, $response);
-} catch (Exception $e) {
-    // Tampilkan error jika inisialisasi kernel gagal
-    echo "<h1>Kernel Error</h1><pre>";
-    echo $e->getMessage();
-    echo "</pre>";
-    exit(1);
-}
+// Menggunakan kernel HTTP standar Laravel 11
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
